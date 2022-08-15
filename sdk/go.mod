374d82ff6aec68b37ce50faa846e618eeb2bc6a2
Monkey <mr.monkey.job@gmail.com>
2022-08-15 17:29:53 +0800

    update rocketmq

--- sdk/go.mod
+++ sdk/go.mod
@@ -3,6 +3,7 @@ module github.com/jxo-me/plus-core/sdk
 go 1.19
 
 require (
+	github.com/apache/rocketmq-client-go/v2 v2.1.1
 	github.com/bsm/redislock v0.7.2
 	github.com/casbin/casbin/v2 v2.51.2
 	github.com/go-redis/redis/v7 v7.4.1
@@ -26,6 +27,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/clbanning/mxj/v2 v2.5.5 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/git-chglog/git-chglog v0.0.0-20190611050339-63a4e637021f // indirect
@@ -33,10 +35,12 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang-jwt/jwt/v4 v4.3.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/golang/mock v1.4.4 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/grokify/html-strip-tags-go v0.0.1 // indirect
 	github.com/imdario/mergo v0.3.7 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -46,19 +50,30 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rabbitmq/amqp091-go v1.4.0 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.4.0 // indirect
+	github.com/stathat/consistent v1.0.0 // indirect
+	github.com/tidwall/gjson v1.13.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/tsuyoshiwada/go-gitcmd v0.0.0-20180205145712-5f1f5f9475df // indirect
 	github.com/urfave/cli v1.20.0 // indirect
 	go.opentelemetry.io/otel v1.7.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.7.0 // indirect
 	go.opentelemetry.io/otel/trace v1.7.0 // indirect
+	go.uber.org/atomic v1.5.1 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/image v0.0.0-20190501045829-6d32002ffd75 // indirect
+	golang.org/x/lint v0.0.0-20190930215403-16217165b5de // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/tools v0.1.7 // indirect
 	gopkg.in/AlecAivazis/survey.v1 v1.8.5 // indirect
 	gopkg.in/kyokomi/emoji.v1 v1.5.1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ sdk/storage/queue/rocketmq.go
@@ -0,0 +1,141 @@
+package queue
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/apache/rocketmq-client-go/v2"
+	"github.com/apache/rocketmq-client-go/v2/consumer"
+	"github.com/apache/rocketmq-client-go/v2/primitive"
+	"github.com/apache/rocketmq-client-go/v2/producer"
+	"github.com/gogf/gf/v2/os/glog"
+	"github.com/gogf/gf/v2/util/gconv"
+	"github.com/jxo-me/plus-core/sdk/storage"
+)
+
+// RocketMQ cache implement
+type RocketMQ struct {
+	Url             []string
+	consumer        rocketmq.PushConsumer
+	consumerOptions *ConsumerOptions
+	producer        rocketmq.Producer
+	producerOptions *ProducerOptions
+	Credentials     primitive.Credentials
+}
+
+func (RocketMQ) String() string {
+	return "rocketmq"
+}
+
+type ConsumerOptions struct {
+	GroupName         string
+	MaxReconsumeTimes int32
+}
+
+type ProducerOptions struct {
+	GroupName  string
+	RetryTimes int
+}
+
+func (r *RocketMQ) newConsumer(ctx context.Context, options *ConsumerOptions) (rocketmq.PushConsumer, error) {
+	if options == nil {
+		r.consumerOptions = &ConsumerOptions{
+			GroupName:         "DEFAULT_CONSUMER",
+			MaxReconsumeTimes: -1,
+		}
+	}
+	return rocketmq.NewPushConsumer(
+		//consumer.WithGroupName("GAME_RECORD"),
+		consumer.WithGroupName(r.consumerOptions.GroupName),
+		consumer.WithNsResolver(primitive.NewPassthroughResolver(r.Url)),
+		consumer.WithConsumerModel(consumer.Clustering),
+		consumer.WithMaxReconsumeTimes(r.consumerOptions.MaxReconsumeTimes),
+		consumer.WithCredentials(r.Credentials),
+		//consumer.WithCredentials(primitive.Credentials{
+		//	AccessKey: "RocketMQ",
+		//	SecretKey: "12345678",
+		//}),
+	)
+}
+
+func (r *RocketMQ) newProducer(ctx context.Context, options *ProducerOptions) (rocketmq.Producer, error) {
+	if options == nil {
+		r.producerOptions = &ProducerOptions{
+			GroupName:  "DEFAULT_CONSUMER",
+			RetryTimes: 3,
+		}
+	}
+	return rocketmq.NewProducer(
+		producer.WithNsResolver(primitive.NewPassthroughResolver(r.Url)),
+		producer.WithRetry(r.producerOptions.RetryTimes),
+		producer.WithCredentials(r.Credentials),
+		//producer.WithCredentials(primitive.Credentials{
+		//	AccessKey: "RocketMQ",
+		//	SecretKey: "12345678",
+		//}),
+		//producer.WithNamespace("namespace"),
+	)
+}
+
+func (r *RocketMQ) Append(ctx context.Context, message storage.Messager) error {
+	rb, err := json.Marshal(message.GetValues())
+	if err != nil {
+		return err
+	}
+	_, err = r.producer.SendSync(
+		ctx,
+		&primitive.Message{
+			Topic: message.GetStream(),
+			Body:  rb,
+		})
+	return err
+}
+
+func (r *RocketMQ) Register(ctx context.Context, name string, f storage.ConsumerFunc) {
+	err := r.consumer.Subscribe(
+		name,
+		consumer.MessageSelector{},
+		func(ctx context.Context, msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
+			for i := range msgs {
+				if len(msgs[i].Body) > 0 {
+					glog.Printf(ctx, "consumed: %v", string(msgs[i].Body))
+					m := new(Message)
+					m.SetValues(gconv.Map(msgs[i].Body))
+					m.SetStream(msgs[i].GetTags())
+					m.SetID(msgs[i].MsgId)
+					err := f(ctx, m)
+					if err != nil {
+						return consumer.Rollback, err
+					}
+				}
+			}
+			return consumer.ConsumeSuccess, nil
+		},
+	)
+	if err != nil {
+		glog.Errorf(ctx, "rocketmq consumer Subscribe error:%v", err)
+		return
+	}
+}
+
+func (r *RocketMQ) Run(ctx context.Context) {
+	err := r.consumer.Start()
+	if err != nil {
+		glog.Warning(ctx, "rocketmq consumer Start error", err)
+	}
+	return
+}
+
+func (r *RocketMQ) Shutdown(ctx context.Context) {
+	if r.producer != nil {
+		err := r.producer.Shutdown()
+		if err != nil {
+			glog.Warning(ctx, "rocketmq producer Close error", err)
+		}
+	}
+	if r.consumer != nil {
+		err := r.consumer.Shutdown()
+		if err != nil {
+			glog.Warning(ctx, "rocketmq consumer Close error", err)
+		}
+	}
+}
