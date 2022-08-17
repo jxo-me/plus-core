e4cea50f8a7ff2cb24cea1f8c10ac24db871dfab
Monkey <mr.monkey.job@gmail.com>
2022-08-17 10:00:21 +0800

    update queue test

---
+++ sdk/storage/queue/redis_test.go
@@ -0,0 +1,137 @@
+package queue
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis/v7"
+	"github.com/robinjoseph08/redisqueue/v2"
+
+	"github.com/jxo-me/plus-core/sdk/storage"
+)
+
+func TestRedis_Publish(t *testing.T) {
+	type fields struct {
+		ConnectOption   *redis.Options
+		ConsumerOptions *redisqueue.ConsumerOptions
+		ProducerOptions *redisqueue.ProducerOptions
+		client          *redis.Client
+		consumer        *redisqueue.Consumer
+		producer        *redisqueue.Producer
+	}
+	type args struct {
+		name    string
+		message storage.Messager
+	}
+	client := redis.NewClient(&redis.Options{})
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			"test01",
+			fields{
+				ConnectOption: &redis.Options{},
+				ConsumerOptions: &redisqueue.ConsumerOptions{
+					VisibilityTimeout: 60 * time.Second,
+					BlockingTimeout:   5 * time.Second,
+					ReclaimInterval:   1 * time.Second,
+					BufferSize:        100,
+					Concurrency:       10,
+					RedisClient:       client,
+				},
+				ProducerOptions: &redisqueue.ProducerOptions{
+					StreamMaxLength:      100,
+					ApproximateMaxLength: false,
+					RedisClient:          client,
+				},
+			},
+			args{
+				name: "test",
+				message: &Message{
+					RoutingKey: "test",
+					Values: map[string]interface{}{
+						"key": "value",
+					},
+				},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if r, err := NewRedis(tt.fields.ProducerOptions, tt.fields.ConsumerOptions); err != nil {
+				t.Errorf("SetQueue() error = %v", err)
+			} else {
+				ctx := context.Background()
+				if err := r.Publish(ctx, tt.args.message); (err != nil) != tt.wantErr {
+					t.Errorf("SetQueue() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			}
+		})
+	}
+}
+
+func TestRedis_Consumer(t *testing.T) {
+	type fields struct {
+		ConnectOption   *redis.Options
+		ConsumerOptions *redisqueue.ConsumerOptions
+		ProducerOptions *redisqueue.ProducerOptions
+		client          *redis.Client
+		consumer        *redisqueue.Consumer
+		producer        *redisqueue.Producer
+	}
+	type args struct {
+		name string
+		f    storage.ConsumerFunc
+	}
+	client := redis.NewClient(&redis.Options{})
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			"test01",
+			fields{
+				ConnectOption: &redis.Options{},
+				ConsumerOptions: &redisqueue.ConsumerOptions{
+					VisibilityTimeout: 60 * time.Second,
+					BlockingTimeout:   5 * time.Second,
+					ReclaimInterval:   1 * time.Second,
+					BufferSize:        100,
+					Concurrency:       10,
+					RedisClient:       client,
+				},
+				ProducerOptions: &redisqueue.ProducerOptions{
+					StreamMaxLength:      100,
+					ApproximateMaxLength: true,
+					RedisClient:          client,
+				},
+			},
+			args{
+				name: "login_log_queue",
+				f: func(ctx context.Context, message storage.Messager) error {
+					fmt.Println("ok")
+					fmt.Println(message.GetValues())
+					return nil
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if r, err := NewRedis(tt.fields.ProducerOptions, tt.fields.ConsumerOptions); err != nil {
+				t.Errorf("SetQueue() error = %v", err)
+			} else {
+				ctx := context.Background()
+				r.Consumer(ctx, tt.args.name, tt.args.f)
+				go r.Run(ctx)
+			}
+		})
+	}
+}
