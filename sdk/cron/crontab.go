4f77147b9ed50ef61221663ef11bade3f68c7218
Monkey <mr.monkey.job@gmail.com>
2022-08-20 17:04:46 +0800

    update cron

---
+++ sdk/cron/crontab.go
@@ -0,0 +1,71 @@
+package cron
+
+import (
+	"context"
+	"github.com/gogf/gf/v2/os/gcron"
+	"github.com/gogf/gf/v2/os/glog"
+)
+
+var insCrontab = crontab{
+	Jobs:    []Job{},
+	Cron:    gcron.New(),
+	Workers: map[string]*gcron.Entry{},
+}
+
+type crontab struct {
+	Jobs    []Job
+	Cron    *gcron.Cron
+	Workers map[string]*gcron.Entry
+}
+
+func Crontab() *crontab {
+	return &insCrontab
+}
+
+func (t *crontab) String() string {
+	return "CrontabService"
+}
+
+func (t *crontab) AddJobs(jobs ...Job) {
+	t.Jobs = jobs
+}
+
+func (t *crontab) GetJobs() []*gcron.Entry {
+	return t.Cron.Entries()
+}
+
+func (t *crontab) StartJob(ctx context.Context, jobName string) bool {
+	if j, ok := t.Workers[jobName]; ok {
+		j.Start()
+		return true
+	}
+	return false
+}
+
+func (t *crontab) StopJob(ctx context.Context, jobName string) bool {
+	if j, ok := t.Workers[jobName]; ok {
+		j.Stop()
+		return true
+	}
+	return false
+}
+
+func (t *crontab) Start(ctx context.Context) {
+	var err error
+	var entry *gcron.Entry
+	for _, job := range t.Jobs {
+		sp := job.GetSpec()
+		entry, err = t.Cron.Add(ctx, sp.Pattern, job.Handle, sp.Name)
+		if err != nil {
+			glog.Debug(ctx, "cron job register error:", err.Error())
+			continue
+		}
+		t.Workers[sp.Name] = entry
+	}
+}
+
+func (t *crontab) Stop(ctx context.Context) {
+	for _, worker := range t.Workers {
+		worker.Stop()
+	}
+}
