85bf4c420b2429f385b6e19eac18289a0255dd62
Monkey <mr.monkey.job@gmail.com>
2022-08-22 17:39:12 +0800

    update applications

--- sdk/task/rabbitmq/rabbitmq.go
+++ sdk/task/rabbitmq/rabbitmq.go
@@ -5,7 +5,9 @@ import (
 	"github.com/gogf/gf/v2/os/glog"
 	"github.com/jxo-me/plus-core/sdk"
 	"github.com/jxo-me/plus-core/sdk/storage"
+	"github.com/jxo-me/plus-core/sdk/storage/queue"
 	"github.com/jxo-me/plus-core/sdk/task"
+	"github.com/jxo-me/rabbitmq-go"
 )
 
 const (
@@ -18,15 +20,10 @@ var insRabbitMq = tRabbitMq{
 }
 
 type tRabbitMq struct {
-	vhost   string
-	Queue   storage.AdapterQueue
 	Routers []task.RabbitMqTask
 }
 
-func Service(vhost ...string) *tRabbitMq {
-	if len(vhost) > 0 {
-		insRabbitMq.vhost = vhost[0]
-	}
+func Service() *tRabbitMq {
 	return &insRabbitMq
 }
 
@@ -41,16 +38,36 @@ func (t *tRabbitMq) AddTasks(tasks ...task.RabbitMqTask) task.RabbitMqService {
 
 func (t *tRabbitMq) Start(ctx context.Context) {
 	glog.Info(ctx, "RabbitMq task start ...")
-	//t.Queue = sdk.Runtime.GetRabbitQueue("vhost") // get rabbitmq instance
-	t.Queue = sdk.Runtime.GetRabbitQueue(t.vhost) // get rabbitmq instance
-	if t.Queue != nil {
-		for _, worker := range t.Routers {
-			spec := worker.GetSpec(ctx)
-			if spec == nil {
-				continue
-			}
+	for _, worker := range t.Routers {
+		spec := worker.GetSpec(ctx)
+		if spec == nil {
+			continue
+		}
+		mQueue := sdk.Runtime.GetRabbitQueue(spec.Vhost) // get rabbitmq instance
+		if mQueue != nil {
 			// Consumer
-			go t.Queue.Consumer(ctx, spec.QueueName, worker.Handle,
+			go mQueue.Consumer(ctx, spec.QueueName, worker.Handle,
+				storage.WithConsumeOptionsBindingRoutingKeys(spec.GetRoutingKeys()),
+				storage.WithConsumeOptionsBindingExchangeName(spec.Exchange),
+				storage.WithConsumeOptionsBindingExchangeType(spec.ExchangeType),
+				storage.WithConsumeOptionsConcurrency(spec.ConsumerNum),
+				storage.WithConsumeOptionsConsumerName(spec.TaskName),
+				storage.WithConsumeOptionsConsumerAutoAck(spec.AutoAck),
+				storage.WithConsumeOptionsQOSPrefetch(QOSPrefetch),
+			)
+		} else {
+			glog.Warning(ctx, "RabbitMq is nil ... NewRabbitMQ ...")
+			dsn := sdk.Runtime.Settings().Settings.Queue.Rabbit.Dsn
+			// get config connection
+			mq, err := queue.NewRabbitMQ(ctx, dsn, &rabbitmq.Config{
+				Vhost: spec.Vhost,
+			})
+			if err != nil {
+				glog.Error(ctx, "NewRabbitMQ error:", err)
+			}
+			sdk.Runtime.SetQueueAdapter(spec.Vhost, mq)
+			// Consumer
+			go mq.Consumer(ctx, spec.QueueName, worker.Handle,
 				storage.WithConsumeOptionsBindingRoutingKeys(spec.GetRoutingKeys()),
 				storage.WithConsumeOptionsBindingExchangeName(spec.Exchange),
 				storage.WithConsumeOptionsBindingExchangeType(spec.ExchangeType),
@@ -60,7 +77,5 @@ func (t *tRabbitMq) Start(ctx context.Context) {
 				storage.WithConsumeOptionsQOSPrefetch(QOSPrefetch),
 			)
 		}
-	} else {
-		glog.Warning(ctx, "RabbitMq is nil ...")
 	}
 }
