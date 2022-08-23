518272925b10e60cde893932da964ee7baace682
Monkey <mr.monkey.job@gmail.com>
2022-08-23 10:29:43 +0800

    update go mod

--- sdk/task/rabbitmq/rabbitmq.go
+++ sdk/task/rabbitmq/rabbitmq.go
@@ -57,7 +57,6 @@ func (t *tRabbitMq) Start(ctx context.Context) {
 			)
 		} else {
 			glog.Warning(ctx, "RabbitMq is nil ... NewRabbitMQ ...")
-			glog.Debug(ctx, "Settings:", sdk.Runtime.Settings())
 			dsn := sdk.Runtime.Settings().Settings.Queue.Rabbit.Dsn
 			// get config connection
 			mq, err := queue.NewRabbitMQ(ctx, dsn, &rabbitmq.Config{
