a032c8af855e82fcc46fadd8b078882ae0c6f4c0
Monkey <mr.monkey.job@gmail.com>
2023-07-24 11:58:28 +0800

    update language

--- sdk/middleware/language.go
+++ sdk/middleware/language.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"context"
 	"github.com/gogf/gf/v2/i18n/gi18n"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"golang.org/x/text/language"
@@ -8,14 +9,20 @@ import (
 
 // Language Set language I18n.
 func Language(r *ghttp.Request) {
-	var preferred []language.Tag
-	var err error
 	ctx := r.GetCtx()
 	lang := r.Get("lang").String()
 	if lang == "" {
 		lang = r.Get("language").String()
 	}
+	ctx = WithLanguage(ctx, lang, r)
+	r.SetCtx(ctx)
+	r.Middleware.Next()
+}
 
+func WithLanguage(ctx context.Context, lang string, r *ghttp.Request) context.Context {
+	var preferred []language.Tag
+	var err error
+	// 语言
 	if lang != "" {
 		preferred, _, err = language.ParseAcceptLanguage(lang)
 		if err != nil {
@@ -23,7 +30,7 @@ func Language(r *ghttp.Request) {
 			lang = "en"
 		}
 	}
-	if preferred == nil {
+	if preferred == nil && r != nil {
 		preferred, _, err = language.ParseAcceptLanguage(r.GetHeader("Accept-Language"))
 		if err != nil {
 			// log err
@@ -40,6 +47,5 @@ func Language(r *ghttp.Request) {
 	base, _ := code.Base()
 	lang = base.String()
 	ctx = gi18n.WithLanguage(ctx, lang)
-	r.SetCtx(ctx)
-	r.Middleware.Next()
+	return ctx
 }
