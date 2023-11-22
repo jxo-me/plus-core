1e0123b98be33945f08751dc31b836cce8b81513
Monkey <mr.monkey.job@gmail.com>
2023-11-22 16:31:44 +0800

    add bucket

--- pkg/bucket.go
+++ pkg/bucket.go
@@ -4,43 +4,43 @@ import "sync"
 
 // Bucket 通用安全桶
 type Bucket[T any] struct {
-	mu    sync.Mutex
-	index int
-	m     map[string]T
+	mu  sync.Mutex
+	Idx int
+	M   map[string]T
 }
 
 func (b *Bucket[T]) Index() int {
-	return b.index
+	return b.Idx
 }
 
 func (b *Bucket[T]) Set(key string, item T) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
-	b.m[key] = item
+	b.M[key] = item
 }
 
 func (b *Bucket[T]) Del(key string) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
-	delete(b.m, key)
+	delete(b.M, key)
 }
 
 func (b *Bucket[T]) Get(key string) T {
 	b.mu.Lock()
 	defer b.mu.Unlock()
-	return b.m[key]
+	return b.M[key]
 }
 
 func (b *Bucket[T]) Len() int {
 	b.mu.Lock()
 	defer b.mu.Unlock()
-	return len(b.m)
+	return len(b.M)
 }
 
 func (b *Bucket[T]) Has(key string) bool {
 	b.mu.Lock()
 	defer b.mu.Unlock()
-	if _, ok := b.m[key]; ok {
+	if _, ok := b.M[key]; ok {
 		return true
 	}
 	return false
