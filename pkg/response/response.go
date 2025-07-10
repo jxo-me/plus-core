ba023f6ce40614c436df50a6101002257f7fc3bb
Mickey <mickey@jxo.me>
2025-07-10 09:13:58 +0800

    update response

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -11,7 +11,6 @@ type JsonRes struct {
 	Code      int         `json:"code" dc:"Status Code" example:"0"` // 错误码((0:成功, 1:失败, >1:错误码))
 	Message   string      `json:"message" dc:"Tips" example:"ok"`    // 提示信息
 	Data      interface{} `json:"data" dc:"Data" example:"{}"`       // 返回数据(业务接口定义具体数据结构)
-	Redirect  string      `json:"redirect,omitempty"`                // 引导客户端跳转到指定路由
 	RequestId string      `json:"request_id,omitempty" dc:"TraceId" example:"8a6f2805b4515ac12058e79e66539be9"`
 }
 
