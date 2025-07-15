a9719a5b41d04e21ed1c87dda2265337004289b6
Mickey <mickey@jxo.me>
2025-07-15 14:18:49 +0800

    update pkg

--- pkg/security/auth/errors.go
+++ pkg/security/auth/errors.go
@@ -1,12 +1,15 @@
 package auth
 
-import "errors"
+import (
+	"github.com/gogf/gf/v2/errors/gcode"
+	"github.com/gogf/gf/v2/errors/gerror"
+)
 
 var (
-	ErrMissingHeader     = errors.New("missing required headers")
-	ErrInvalidTimestamp  = errors.New("invalid or expired timestamp")
-	ErrInvalidAPIKey     = errors.New("invalid api key")
-	ErrSignatureMismatch = errors.New("signature mismatch")
-	ErrReplayAttack      = errors.New("replay attack detected")
-	ErrIPBanned          = errors.New("client ip is banned")
+	ErrMissingHeader     = gerror.NewCode(gcode.New(210, "missing required headers", nil))
+	ErrInvalidTimestamp  = gerror.NewCode(gcode.New(211, "invalid or expired timestamp", nil))
+	ErrInvalidAPIKey     = gerror.NewCode(gcode.New(212, "invalid api key", nil))
+	ErrSignatureMismatch = gerror.NewCode(gcode.New(213, "signature mismatch", nil))
+	ErrReplayAttack      = gerror.NewCode(gcode.New(214, "replay attack detected", nil))
+	ErrIPBanned          = gerror.NewCode(gcode.New(215, "client ip is banned", nil))
 )
